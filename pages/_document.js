21403882de53ff4d20d3a2de7ca5b37a12dedc3a
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2023-01-12 16:34:05 +0100

    🎉 first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html className="h-full antialiased" lang="en">
+      <Head>
+        <link rel="icon" href="https://emojicdn.elk.sh/🔮" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
